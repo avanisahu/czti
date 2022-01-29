990012d164c5abd693a276161cb68ba7a11c98d4
avanisahu <88477346+avanisahu@users.noreply.github.com>
2022-01-30 00:26:33 +0530

    Update cztifile.py

--- cztifile.py
+++ cztifile.py
@@ -1,12 +1,11 @@
 dataset=('/home/avani/Desktop/czti/czti_sample_data/20160331_T01_112T01_9000000406_level2/modeM0/AS1T01_112T01_9000000406cztM0_level2_bc.evt')
 from astropy.io import fits
 hdu=fits.open(dataset)
-hdu
-hdu[1].header
-hdu[1].data
-hdu[1].data["TIME"]
-import matplotlib.pyplot as plt
-x=[1.97325456e+08, 1.97325456e+08, 1.97325456e+08, ...,
-       1.97326754e+08, 1.97326754e+08, 1.97326754e+08]
-plt.hist(x, bins = 175)    
+from matplotlib import pyplot as plt
+import numpy as np
+x=hdu[1].data["TIME"]
+import math
+w = 25
+n = math.ceil((x.max() - x.min())/w)
+ax = plt.hist(x, bins = n)
 plt.show()
